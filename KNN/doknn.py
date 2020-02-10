f6f21f03ec0343545abbc220882e9d5a538b0765
Mehran Meidani <m.meidani@me.com>
2020-02-10 15:49:46 -0500

    Add util folder

--- KNN/doknn.py
+++ KNN/doknn.py
@@ -1,26 +1,73 @@
+import sys
+# Include util folder for future use
+sys.path.insert(1, '../Util')
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import numpy
+import matplotlib.pyplot as plt
+from plot_correlation import plot_correlation, plot_densities, plot_hist
+from sklearn import preprocessing
+
 import csv
 import os
-import sys
+
 
 class DataSet():
     data = []
     featureNames = []
     target = []
-    targetNames = []
 
     def addTrainingData(self, attributes, targetValue):
         self.data.append(attributes)
         self.target.append(targetValue)
 
-def doKNN(csvFilePath, ):
-    with open(os.path.join('.', '7146875/IST_MIR.csv'), 'r') as csvFile:
+    def finalizeDataset(self):
+        self.data = numpy.array(self.data, dtype=numpy.float64)
+        # scaler = preprocessing.QuantileTransformer(output_distribution='normal', random_state=0)
+        # self.data = scaler.fit_transform(self.data)
+    
+    def setFeatureNames(self, featureNames):
+        self.featureNames = featureNames
+
+def doKNN(csvFilePath, featureColumns, targetColumn):
+    # plot_correlation(csvFilePath, featureColumns + [targetColumn])
+    # plot_densities(csvFilePath, featureColumns + [targetColumn], targetColumn)
+    dataSet = DataSet()
+    with open(os.path.join('.', csvFilePath), 'r') as csvFile:
+        dataSet.setFeatureNames(featureColumns)
         csvReader = csv.DictReader(csvFile)
         for row in csvReader:
-            pass
+            newTrainingDataRow = []
+            for feature in featureColumns :
+                newTrainingDataRow.append(row[feature])
+            dataSet.addTrainingData(newTrainingDataRow, row[targetColumn])
+    
+    dataSet.finalizeDataset()    
+    # plot_hist(dataSet.data)
+    X_train,X_test,y_train,y_test = train_test_split(dataSet.data,dataSet.target,test_size=0.2,random_state=4)
+    k_range = range(1,26)
+    scores = {}
+    scores_list = []
+    for k in k_range:
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(X_train,y_train)
+        y_pred=knn.predict(X_test)
+        scores[k] = metrics.accuracy_score(y_test,y_pred)
+        scores_list.append(metrics.accuracy_score(y_test,y_pred))
+        # print(metrics.classification_report(y_test, y_pred))
+    
+    plt.plot(k_range,scores_list)
+    plt.xlabel('Value of K for KNN')
+    plt.ylabel('Testing Accuracy')
+    plt.show()
+    
 
 def main(argv):
     csvFilePath = argv[1]
-
+    csvFeatureColumns = argv[2].split(',')
+    csvTargetColumn = argv[3]
+    doKNN(os.path.join('.', csvFilePath), csvFeatureColumns, csvTargetColumn)
 
 if __name__ == "__main__":
     main(sys.argv)
---
+++ Util/plot_correlation.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import numpy as np
+
+def plot_correlation(filePath, featureColumns):
+    '''
+    plot correlation's matrix to explore dependency between features 
+    '''
+    # init figure size
+    data  = pd.read_csv(filePath, usecols=featureColumns)
+    fig = plt.figure()
+    sns.heatmap(data.corr(), annot=True, fmt=".2f")
+    plt.show()
+
+def plot_hist(data):
+    # x = [item[2] for item in data]
+    # print(np.log1p(x))
+    # plt.hist(np.log1p(x))
+    scaler = preprocessing.QuantileTransformer(output_distribution='normal', random_state=0)
+    new_data = scaler.fit_transform(data)
+    x = [item[2] for item in new_data]
+    print(plt.hist(x))
+
+def plot_densities(filePath, featureColumns, targetColumn):
+    '''
+    Plot features densities depending on the outcome values
+    '''
+
+    data  = pd.read_csv(filePath, usecols=featureColumns)
+    # change fig size to fit all subplots beautifully 
+    # rcParams['figure.figsize'] = 15, 20
+
+    # separate data based on outcome values 
+    outcome_0 = data[data[targetColumn] == 0]
+    outcome_1 = data[data[targetColumn] == 1]
+
+    # init figure
+    fig, axs = plt.subplots(len(featureColumns), 1)
+    fig.suptitle('Features densities for different outcomes 0/1')
+    plt.subplots_adjust(left = 0.25, right = 0.9, bottom = 0.1, top = 0.95,
+                        wspace = 0.2, hspace = 0.9)
+
+    # plot densities for outcomes
+    names = featureColumns
+    for column_name in names[:-1]: 
+        ax = axs[names.index(column_name)]
+        #plt.subplot(4, 2, names.index(column_name) + 1)
+        outcome_0[column_name].plot(kind='density', ax=ax, subplots=True, 
+                                    sharex=False, color="red", legend=True,
+                                    label=column_name + ' for Outcome = 0')
+        outcome_1[column_name].plot(kind='density', ax=ax, subplots=True, 
+                                     sharex=False, color="green", legend=True,
+                                     label=column_name + ' for Outcome = 1')
+        ax.set_xlabel(column_name + ' values')
+        ax.set_title(column_name + ' density')
+        ax.grid('on')
+    plt.show()
