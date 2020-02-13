74d505c2a62b1e3ba489e9482475acf9130b2bfb
Mehran Meidani <m.meidani@me.com>
2020-02-13 16:04:04 -0500

    Complete KNN

--- analyze.py
+++ analyze.py
@@ -1,5 +1,12 @@
 from PCA.doPCA import applyPCA
 from KNN.doknn import doKNN
 
-dataSet = applyPCA('Wikimedia', '.')
-doKNN(dataSet)
+dataSetMIR = applyPCA('Mirantis', '.')
+dataSetMOZ = applyPCA('Mozilla', '.')
+dataSetOST = applyPCA('Openstack', '.')
+dataSetWIK = applyPCA('Wikimedia', '.')
+
+print("Mirantis:", doKNN(dataSetMIR))
+print("Mozilla:", doKNN(dataSetMOZ))
+print("Openstack:", doKNN(dataSetOST))
+print("Wikimedia:", doKNN(dataSetWIK))
