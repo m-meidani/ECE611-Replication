a0c2f90c7442a73dce4ce0059cf8ff0e8051a6a3
rey1 <reihane.kamali@gmail.com>
2020-03-04 17:08:27 -0500

    Update doknn.py

--- KNN/doknn.py
+++ KNN/doknn.py
@@ -14,26 +14,26 @@ def doKNN(X_train, X_test, y_train, y_test):
     # X_train,X_test,y_train,y_test = train_test_split(knnData,dataSet.target,test_size=0.2,random_state=4)
     cv = KFold(n_splits=10)
     #k_range = range(1, 26)
-    k_range = 3
+    k=3
     scores_list = []
 
     precision_score = []
     auc_score = []
     recall_score = []
     f1_score = []
-    for k in k_range:
-        knn = KNeighborsClassifier(n_neighbors=k)
-        knn.fit(X_train, y_train)
-        y_pred = knn.predict(X_test)
-        scores_list.append(metrics.accuracy_score(y_test, y_pred))
-        precision_score.append(metrics.precision_score(y_test, y_pred))
-        try:
-            auc_score.append(metrics.roc_auc_score(y_test, y_pred))
-        except:
-            auc_score.append(0)
+    #for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    scores_list.append(metrics.accuracy_score(y_test, y_pred))
+    precision_score.append(metrics.precision_score(y_test, y_pred))
+    try:
+        auc_score.append(metrics.roc_auc_score(y_test, y_pred))
+    except:
+        auc_score.append(0)
 
-        recall_score.append(metrics.recall_score(y_test, y_pred))
-        f1_score.append(metrics.f1_score(y_test, y_pred))
+    recall_score.append(metrics.recall_score(y_test, y_pred))
+    f1_score.append(metrics.f1_score(y_test, y_pred))
 
         # print(metrics.classification_report(y_test, y_pred))
     return {'precision': max(precision_score), 'accuracy': max(scores_list), 'auc': max(auc_score), 'recall': max(recall_score), 'f1_measure': max(f1_score), 'algorithm' : knn}
